6336a64f459990e88633df8407f789af6890fdc8
Drew Reutlinger <drew.reutlinger@gmail.com>
2025-12-06 14:49:17 -0700

    first commit

---
+++ functions.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * wyodrew Theme Functions
+ *
+ * FSE themes don't need traditional theme support declarations.
+ * Most functionality is handled by theme.json.
+ */
+
+/**
+ * Enqueue theme styles
+ */
+function wyodrew_enqueue_styles() {
+    wp_enqueue_style( 'wyodrew-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version') );
+}
+add_action( 'wp_enqueue_scripts', 'wyodrew_enqueue_styles' );
+
+/**
+ * Register custom block styles
+ */
+function wyodrew_register_block_styles() {
+    register_block_style(
+        'core/group',
+        array(
+            'name'  => 'tech-card',
+            'label' => 'Tech Card',
+        )
+    );
+}
+add_action( 'init', 'wyodrew_register_block_styles' );
+
+/**
+ * Register Custom Post Type: Projects
+ */
+function wyodrew_register_projects_cpt() {
+    $labels = array(
+        'name'                  => 'Projects',
+        'singular_name'         => 'Project',
+        'menu_name'             => 'Projects',
+        'add_new'               => 'Add New',
+        'add_new_item'          => 'Add New Project',
+        'edit_item'             => 'Edit Project',
+        'new_item'              => 'New Project',
+        'view_item'             => 'View Project',
+        'search_items'          => 'Search Projects',
+        'not_found'             => 'No projects found',
+        'not_found_in_trash'    => 'No projects found in trash',
+        'all_items'             => 'All Projects',
+    );
+
+    $args = array(
+        'labels'                => $labels,
+        'public'                => true,
+        'has_archive'           => true,
+        'show_in_rest'          => true,
+        'rest_base'             => 'projects',
+        'menu_icon'             => 'dashicons-portfolio',
+        'supports'              => array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields', 'page-attributes' ),
+        'rewrite'               => array( 'slug' => 'project' ),
+        'show_in_nav_menus'     => true,
+        'menu_position'         => 5,
+    );
+
+    register_post_type( 'project', $args );
+}
+add_action( 'init', 'wyodrew_register_projects_cpt' );
+
+/**
+ * Register Custom Taxonomy: Project Categories
+ */
+function wyodrew_register_project_category_taxonomy() {
+    $labels = array(
+        'name'              => 'Project Categories',
+        'singular_name'     => 'Project Category',
+        'search_items'      => 'Search Project Categories',
+        'all_items'         => 'All Project Categories',
+        'edit_item'         => 'Edit Project Category',
+        'update_item'       => 'Update Project Category',
+        'add_new_item'      => 'Add New Project Category',
+        'new_item_name'     => 'New Project Category Name',
+        'menu_name'         => 'Categories',
+    );
+
+    $args = array(
+        'labels'            => $labels,
+        'hierarchical'      => true,
+        'public'            => true,
+        'show_in_rest'      => true,
+        'rest_base'         => 'project-categories',
+        'show_admin_column' => true,
+        'rewrite'           => array( 'slug' => 'project-category' ),
+    );
+
+    register_taxonomy( 'project-category', array( 'project' ), $args );
+}
+add_action( 'init', 'wyodrew_register_project_category_taxonomy' );
